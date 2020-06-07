29c902ce65edd6310084b67b6816a6a2426c9ddb
eddylogo123 <trilogianet@gmail.com>
2020-06-07 18:25:20 -0400

    modificaciones de rama

--- src/app/components/parques/parques.component.ts
+++ src/app/components/parques/parques.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+//importar modulos
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-parques',
@@ -7,19 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParquesComponent implements OnInit {
 
-    public nombre: string;
-    public metros: number;
+    //recibir desde un componente padre
+    @Input() nombre: string;
+
+    //recibir desde un componente padre, considerándose un alias
+    @Input('metros_cuadrados') metros: number;
+
     public vegetacion: string;
     public abierto: boolean;
 
+    //informacion para sacar datos hacia el padre
+    @Output() pasameLosDatos = new EventEmitter();
+
   constructor() {
     this.nombre = 'Parque natural para caballos';
-    this. metros = 450;
+    this.metros = 450;
     this.vegetacion = 'Alta';
     this.abierto = true;
   }
 
   ngOnInit(): void {
+  }
+
+  emitirEvento(event){
+  this.pasameLosDatos.emit({
+    'nombre': this.nombre,
+    'metros': this.metros,
+    'vegetacion': this.vegetacion,
+    'abierto': this.abierto
+  });
   }
 
 }
