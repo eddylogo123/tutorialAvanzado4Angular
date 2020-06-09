6daa31b0fb3a6df2a06e54ea43be117014dfe627
eddylogo123 <trilogianet@gmail.com>
2020-06-09 19:39:09 -0400

    Aprendiendo Sessiones y LocalStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component, DoCheck } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements  DoCheck{
+export class AppComponent implements DoCheck, OnInit{
   title = 'curso-angular4-avanzado';
+  emailContacto: string;
 
   ngDoCheck(): void {
-    console.log('El DOCHECK  se ha ejecutado...');
+    // console.log('El DOCHECK  se ha ejecutado...');
+    this.emailContacto = localStorage.getItem('emailContacto');
+  }
+
+  ngOnInit(): void {
+    // console.log(localStorage.getItem('emailContacto'));
+    this.emailContacto = localStorage.getItem('emailContacto');
+  }
+
+  borrarEmail(){
+    localStorage.removeItem('emailContacto');
+    localStorage.clear();
+    this.emailContacto = null;
   }
 }
